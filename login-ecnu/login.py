33d85f74322aaf1ba46b55e0f9d4b2f497dddc99
TXXT <hunterlxt@live.com>
2019-05-07 19:25:11 +0800

    Update login.py

--- login-ecnu/login.py
+++ login-ecnu/login.py
@@ -10,7 +10,6 @@ PW = "password"
 
 postbody = "action=login&username={}&password={}&ac_id=1&user_ip=&nas_ip=&user_mac=&save_me=0&ajax=1".format(UN, PW).encode('utf-8')
 retbytes = urllib.request.urlopen("https://login.ecnu.edu.cn/include/auth_action.php", postbody).read()
-print(retbytes)
 if (re.findall(r"login_ok".encode("utf-8"), retbytes)):
     print("Login successfully!")
 else:
